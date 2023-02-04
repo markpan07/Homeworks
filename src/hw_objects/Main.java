8ba49da548ea0e8d33aeebe47b6dc466410b49cf
МАРК <markpan07@mail.ru>
2023-02-04 15:12:33 +0300

    5 points are done

---
+++ src/hw_objects/Main.java
@@ -0,0 +1,11 @@
+package hw_objects;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Author lev = new Author("Lev", "Tolstoy");
+        Book book1 = new Book("nakazanie", lev, 1975);
+
+
+    }
+}

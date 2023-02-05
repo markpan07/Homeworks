716500620fbdd0f703b5602dd4751dc820fb2862
МАРК <markpan07@mail.ru>
2023-02-05 19:45:20 +0300

    Homework methods2 is done

--- src/hw_objects/Book.java
+++ src/hw_objects/Book.java
@@ -1,8 +1,10 @@
 package hw_objects;
 
+import java.util.Objects;
+
 public class Book {
-    private String name;
-    private Author author;
+    private final String name;
+    private final Author author;
     private int year;
 
     public Book(String name, Author author, int year) {
@@ -12,7 +14,7 @@ public class Book {
 
     }
 
-    public String getName (){
+    public String getName() {
         return this.name;
     }
 
@@ -24,7 +26,32 @@ public class Book {
         return this.year;
     }
 
-    public void setYear (int year) {
+    public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Название: " + name + "\n" +
+                "Автор: " + author + "\n" +
+                "Год издания: " + year + "\n";
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return author.equals(book.author) && getName().equals(book.getName()) && getYear() == book.getYear();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, year, author);
     }
 }
--- src/hw_objects/Main.java
+++ src/hw_objects/Main.java
@@ -5,9 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Author levTolstoy = new Author("Lev", "Tolstoy");
         Author fedorDostoevskiy = new Author("Fedor", "Dostoevskiy");
+        Author fedorDostoevskiy2 = new Author("Fedor", "Dostoevskiy");
         Book idiot = new Book ("Idiot", fedorDostoevskiy, 1852);
         Book besy = new Book ("Besy", fedorDostoevskiy, 1850);
-        idiot.setYear(1860);
+        Book besy1 = new Book ("Besy", fedorDostoevskiy, 1850);
 
+        /*System.out.println(fedorDostoevskiy2.equals(fedorDostoevskiy));
+        System.out.println(idiot.hashCode());
+        System.out.println(besy.hashCode());
+        System.out.println(besy1.hashCode());
+        System.out.println(besy1.equals(besy));
+        System.out.println(idiot.toString());
+        System.out.println();
+        System.out.println(besy.toString());
+        System.out.println(besy.equals(besy1));
+        System.out.println(besy.hashCode());*/
     }
 }
